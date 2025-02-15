e2626dc5e01a7ced0ff4c666be3a037b90df0c82
Felipyasha <felipe_nascalves@hotmail.com>
2025-02-15 15:47:43 -0300

    feat: lesson 5

--- app-aula-react/src/App.js
+++ app-aula-react/src/App.js
@@ -1,22 +1,59 @@
 
 import { useState } from 'react';
-import Name from './components/Name'
 
-function App(){
-  const [dev, setDev] = useState('Yasha');
+function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
 
-  function changeName(nome){
-    setDev(nome);
+  const [user, setUser] = useState({});
+
+  function handleRegister(e) {
+    e.preventDefault();
+    setUser({
+      name: name,
+      age: age,
+      email: email,
+    })
   }
 
   return (
     <div>
-      <h1>Mim de um emprego papai, tô me esforçando!!</h1>
-      <h2>Sou eu o grande {dev}</h2>
-      <button onClick={ () => changeName('Felipyasha') }>
-        Trocar Dev
-      </button>
-      
+      <h1>Cadastro de Usuário</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome: </label><br/>
+        <input 
+          placeholder="Digite seu nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        /><br/>
+
+        <label>Email: </label><br/>
+        <input 
+          placeholder="Digite seu Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br/>
+
+        <label>Idade: </label><br/>
+        <input 
+          placeholder="Digite sua Idade"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        /><br/>
+
+        <button type="submit">Registrar</button>
+
+        <br/>
+        <br/>
+
+        <div>
+          <span>Bem vindo {user.name}</span><br/>
+          <span>Email:  {user.email}</span><br/>
+          <span>Idade:  {user.age}</span><br/>
+        </div>
+      </form>
     </div>
   );
 }
