6bc62f9ee8bf38c6580c2c9b73f3015790e30c61
Felipyasha <felipe_nascalves@hotmail.com>
2025-03-26 18:30:54 -0300

    feat: lesson 10

--- app-aula-react/src/rotas.js
+++ app-aula-react/src/rotas.js
@@ -4,6 +4,8 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
+import Produto from "./pages/Produto";
+import Erro from './pages/Erro'
 
 const Rotas = () => {
     return(
@@ -12,6 +14,9 @@ const Rotas = () => {
                 <Route path="/" element={<Home />}> </Route>
                 <Route path="/sobre" element={<Sobre />}> </Route>
                 <Route path="/contato" element={<Contato />}> </Route>
+                <Route path="/produto/:id" element={<Produto />}> </Route>
+
+                <Route path="/*" element={<Erro />}> </Route>
            </Routes>
         </BrowserRouter>
     )
