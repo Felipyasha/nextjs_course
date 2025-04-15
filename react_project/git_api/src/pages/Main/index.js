7cee8f194441c0370196c15bbb8183740cb4a855
Felipyasha <felipe_nascalves@hotmail.com>
2025-04-15 15:10:58 -0300

    feat: react project - lesson 2

--- react_project/git_api/src/pages/Main/index.js
+++ react_project/git_api/src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import  React  from 'react';
 
+import { Title } from './styles';
+
 export default function Main(){
     return(
-        <h1>
+        <Title>
             Main
-        </h1>
+        </Title>
     );
 }
