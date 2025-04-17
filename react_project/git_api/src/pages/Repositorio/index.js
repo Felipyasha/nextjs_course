ddbe13200b1eb09dab1d34c53ace900f8eb1bac7
Felipyasha <felipe_nascalves@hotmail.com>
2025-04-17 18:57:02 -0300

    feat: react project - lesson 11

--- react_project/git_api/src/pages/Repositorio/index.js
+++ react_project/git_api/src/pages/Repositorio/index.js
@@ -1,45 +1,70 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import api from '../../services/api';
 import { useParams } from 'react-router-dom';
-import { Container } from './styles';
+import { Container, Owner, Loading, BackButton} from './styles';
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function Repositorio() {
 
-    const params = useParams();
-    const [repository, setRepository] = useState({});
-    const [issues, setIssues] = useState([]);
-    const [loading, setloading] = useState(true);
+  const params = useParams();
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setloading] = useState(true);
 
-    useEffect(() => {
+  useEffect(() => {
 
-        async function load() {
-            const nomeRepo = params.repositorio;
+    async function load() {
+      const nomeRepo = params.repositorio;
 
-            const [repositorioData, issuesData] = await Promise.all([
-                api.get(`/repos/${nomeRepo}`),
-                api.get(`/repos/${nomeRepo}/issues`, {
-                    params:{
-                        state: 'open',
-                        per_page: 5
-                    }
-                })
-            ]);
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
 
-            
 
-            setRepository(repositorioData.data);
-            setIssues(issuesData.data);
-            setloading(false);
 
-        }
+      setRepository(repositorioData.data);
+      setIssues(issuesData.data);
+      setloading(false);
 
-        load();
+    }
 
-    }, [params.repositorio]);
+    load();
 
+  }, [params.repositorio]);
+
+  if (loading) {
     return (
-        <Container>
+      <Loading>
+        <h1>
+          Carregando
+        </h1>
+      </Loading>
+    )
+  }
+  return (
+    <Container>
+      <BackButton to="/">
+        <FaArrowLeft color='#000' size={30}/>
+      </BackButton>
 
-        </Container>
-    );
+      <Owner>
+        <img
+          src={repository.owner.avatar_url}
+          alt={repository.owner.login }
+        />
+        <h1>
+          {repository.name}
+        </h1>
+        <p>
+          {repository.description}
+        </p>
+      </Owner>
+    </Container>
+  );
 }
