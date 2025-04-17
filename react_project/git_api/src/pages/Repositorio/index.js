8f5c449e825207e8654fdb3ca10b3a5e1500a6a0
Felipyasha <felipe_nascalves@hotmail.com>
2025-04-17 18:30:26 -0300

    feat: react project - lesson 10

--- react_project/git_api/src/pages/Repositorio/index.js
+++ react_project/git_api/src/pages/Repositorio/index.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import api from '../../services/api';
 import { useParams } from 'react-router-dom';
+import { Container } from './styles';
 
 export default function Repositorio() {
 
     const params = useParams();
+    const [repository, setRepository] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setloading] = useState(true);
+
+    useEffect(() => {
+
+        async function load() {
+            const nomeRepo = params.repositorio;
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params:{
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            
+
+            setRepository(repositorioData.data);
+            setIssues(issuesData.data);
+            setloading(false);
+
+        }
+
+        load();
+
+    }, [params.repositorio]);
 
     return (
-        <h1 style={{ color: '#FFF' }}>
-            {decodeURIComponent(params.repositorio)}
-        </h1>
+        <Container>
+
+        </Container>
     );
 }
